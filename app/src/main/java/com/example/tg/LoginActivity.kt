3b1a8d4961149f410dc59186af39856d015698b8
Tamie <giseletamie@hotmail.com>
2019-08-02 01:32:20 -0300

    Second commit

--- app/src/main/java/com/example/tg/LoginActivity.kt
+++ app/src/main/java/com/example/tg/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.example.tg
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() , LoginFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogEntrar.setOnClickListener {
+            supportFragmentManager.inTransaction {
+                add(R.id.frmLoginContainer, LoginFragment())
+            }
+            btnLogEntrar.visibility = View.INVISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/tg/MainActivity.kt
+++ app/src/main/java/com/example/tg/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tg
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
